a1c59eaf0175e0d7e54029461eb715d5c08678e7
PENG CAI <brucecp827@gmail.com>
2014-11-12 21:15:19 -0800

    FBLogIn With Swift

---
+++ HW9/HW9/Bridge-Header.h
@@ -0,0 +1,15 @@
+//
+//  Bridge-Header.h
+//  HW9
+//
+//  Created by Bruce Campbell on 11/12/14.
+//  Copyright (c) 2014 Bruce Campbell. All rights reserved.
+//
+
+#ifndef HW9_Bridge_Header_h
+#define HW9_Bridge_Header_h
+
+#import <FacebookSDK/FacebookSDK.h>
+
+
+#endif
---
+++ HW9/HW9/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  HW9
+//
+//  Created by Bruce Campbell on 11/12/14.
+//  Copyright (c) 2014 Bruce Campbell. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController, FBLoginViewDelegate {
+    @IBOutlet var fbLoginView : FBLoginView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.fbLoginView.delegate = self
+        self.fbLoginView.readPermissions = ["public_profile", "email", "user_friends"]
+        
+    }
+    
+    
+    
+    func loginViewShowingLoggedInUser(loginView : FBLoginView!) {
+        println("User Logged In")
+    }
+    
+    func loginViewFetchedUserInfo(loginView : FBLoginView!, user: FBGraphUser) {
+        println("User: \(user)")
+        println("User ID: \(user.objectID)")
+        println("User Name: \(user.name)")
+        var userEmail = user.objectForKey("email") as String
+        println("User Email: \(userEmail)")
+    }
+    
+    func loginViewShowingLoggedOutUser(loginView : FBLoginView!) {
+        println("User Logged Out")
+    }
+    
+    func loginView(loginView : FBLoginView!, handleError:NSError) {
+        println("Error: \(handleError.localizedDescription)")
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
